4bc70ae0c015cd82eea39e3b87b6c3ad20c3a217
Vincent <124529788+BerlinVincent@users.noreply.github.com>
2025-01-20 08:31:59 +0100

    Commented Alignment.hpp

--- Dungeons&Dragons/Framework/Alignment.hpp
+++ Dungeons&Dragons/Framework/Alignment.hpp
@@ -1,5 +1,8 @@
 #include "inclusions.hpp"
 
+/**
+* @brief An enum for the different moral Alignments of Dungeons&Dragons
+*/
 enum Alignment {
     LAWFULGOOD,     NEUTRALGOOD,    CHAOTICGOOD,
     LAWFULNEUTRAL,  TRUENEUTRAL,    CHAOTICNEUTRAL,
