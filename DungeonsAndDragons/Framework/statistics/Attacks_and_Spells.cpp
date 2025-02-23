8336672160e51c553f0304728e80d4697ee4282d
BerlinVincent <vincent.sternberg@gmail.com>
2025-02-23 10:44:02 +0100

    implement get functions for attacks

--- DungeonsAndDragons/Framework/statistics/Attacks_and_Spells.cpp
+++ DungeonsAndDragons/Framework/statistics/Attacks_and_Spells.cpp
@@ -1,1 +1,33 @@
 #include "Attacks_and_Spells.hpp"
+
+auto Attack::getName() -> string {
+    return name;
+}
+
+auto Attack::getAtkBonus() -> int {
+    return attack_bonus;
+}
+
+auto Attack::getDamageRange() -> RANGE {
+    return damage_range;
+}
+
+auto Attack::getType() -> DamageType {
+    return type;
+}
+
+auto Attack::getTargetType() -> TargetType {
+    return AoE;
+}
+
+auto Attack::getReach() -> int {
+    return reach;
+}
+
+auto Attack::getActionType() -> int {
+    return action_type;
+}
+
+auto Attack::getSpellslot() -> int {
+    return spellslot;
+}
